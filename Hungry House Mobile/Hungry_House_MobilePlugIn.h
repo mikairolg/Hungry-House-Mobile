ad8a3d6f0d4c7bd23eb435cd4b42cc862f266ff6
Gloria Kim <gloriakim@C02NK17MG3QN.local>
2014-11-24 12:23:08 +0100

    Initial Commit

---
+++ Hungry House Mobile/Hungry_House_MobilePlugIn.h
@@ -0,0 +1,17 @@
+//
+//  Hungry_House_MobilePlugIn.h
+//  Hungry House Mobile
+//
+//  Created by Gloria Kim on 24/11/14.
+//  Copyright (c) 2014 Delivery Hero. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface Hungry_House_MobilePlugIn : QCPlugIn
+
+// Declare here the properties to be used as input and output ports for the plug-in e.g.
+//@property double inputFoo;
+//@property (copy) NSString* outputBar;
+
+@end
